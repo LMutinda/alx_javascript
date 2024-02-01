86cdbc8e143aa40e1b2784771151bc18b32b1ff1
LMutinda <lornamutinda3@gmail.com>
2024-02-01 23:45:50 +0300

    js

---
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,38 @@
+// Rectangle.js
+class Rectangle {
+    constructor(w, h) {
+      if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+        // If w or h is equal to 0 or not a positive integer, create an empty object
+        return {};
+      }
+  
+      // Initialize the instance attributes width and height
+      this.width = w;
+      this.height = h;
+    }
+  
+    // Instance method to print the rectangle using the character X
+    print() {
+      if (this.width && this.height) {
+        for (let i = 0; i < this.height; i++) {
+          console.log('X'.repeat(this.width));
+        }
+      }
+    }
+  
+    // Instance method to exchange the width and height of the rectangle
+    rotate() {
+      const temp = this.width;
+      this.width = this.height;
+      this.height = temp;
+    }
+  
+    // Instance method to double the width and height of the rectangle
+    double() {
+      this.width *= 2;
+      this.height *= 2;
+    }
+  }
+  
+  module.exports = Rectangle;
+  
