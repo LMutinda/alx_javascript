8a184ecde080c487eb4715266cc12a991d56dcab
LMutinda <lornamutinda3@gmail.com>
2024-02-07 19:40:20 +0300

    js

--- javascript-network/4-completed_tasks.js
+++ javascript-network/4-completed_tasks.js
@@ -34,7 +34,7 @@ request.get(apiUrl, (error, response, body) => {
     // Output the results
     
     // Convert the object to a JSON string with single quotes
-    const jsonString = JSON.stringify(completedTasksByUserId).replace(/"/g, "'");
+    const jsonString = JSON.stringify(completedTasksByUserId, null, 2).replace(/"/g, "'");
 
     // Print the JSON string
     console.log(jsonString);
