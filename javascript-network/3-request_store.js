92bf256e3df55c4e47f55e6e0e33a3a69e04b447
LMutinda <lornamutinda3@gmail.com>
2024-02-07 19:03:58 +0300

    js

---
+++ javascript-network/3-request_store.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+
+// Get the URL and file path from the command line arguments
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+// Make a GET request to the specified URL
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+  } else if (response.statusCode !== 200) {
+    console.error('Status:', response.statusCode);
+  } else {
+    // Write the response body to the file
+    fs.writeFile(filePath, body, 'utf-8', (err) => {
+      if (err) {
+        console.error('Error writing to file:', err);
+      } else {
+        console.log(`Successfully saved the contents of ${url} to ${filePath}`);
+      }
+    });
+  }
+});
